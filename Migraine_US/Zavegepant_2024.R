ef78cbb37b13f02c753c718dee2973fd657930fe
Paulo Bastos <91428363+Le-bruit-de-nos-pas@users.noreply.github.com>
2024-04-15 17:46:21 +0200

    Add files via upload

--- Migraine_US/Zavegepant_2024.R
+++ Migraine_US/Zavegepant_2024.R
@@ -7,7 +7,6 @@ library(spatstat)
 library(lubridate)
 library("readxl")
 
-
 options(scipen = 999)
 
 # % With Oral/Inj CGRP or Nasal/triptan Spray -----------------------
@@ -2177,3 +2176,39 @@ df %>%
 
 
 # -----------
+
+# In which months are patients using triptan sprays ? ------------
+Drug_formulary <- fread("Source/Drug_formulary_NS.txt")
+data.frame(Drug_formulary)
+data.frame(Drug_formulary %>% select(drug_class, drug_group) %>% distinct())
+
+string_Triptan_Nasal <- paste0("\\b(",paste0(Drug_formulary$drug_id[Drug_formulary$drug_group_2=="Nasal Spray" &
+                                                                      Drug_formulary$drug_group=="Triptans"], collapse = "|"),")\\b")
+
+
+MIGUS24_Doses_version_NS <- fread("Source/MIGUS24 Doses - version NS.txt")
+names(MIGUS24_Doses_version_NS)
+unique(MIGUS24_Doses_version_NS$status)
+
+MIGUS24_Doses_version_NS <- MIGUS24_Doses_version_NS %>% select(drug_id_2, patid, weight, from_dt, days_sup, qty) %>% distinct()
+
+range(MIGUS24_Doses_version_NS$from_dt)
+
+MIGUS24_Doses_version_NS <- MIGUS24_Doses_version_NS %>% filter(grepl(string_Triptan_Nasal, drug_id_2))
+
+MIGUS24_Doses_version_NS
+
+
+MIGUS24_Doses_version_NS %>% 
+  mutate(from_dt=str_sub(as.character(from_dt), 6L,7L)) %>%
+  group_by(from_dt) %>% summarise(tot=sum(weight))
+
+
+data.frame(MIGUS24_Doses_version_NS %>% 
+  mutate(from_dt=str_sub(as.character(from_dt), 1L,7L)) %>%
+  group_by(from_dt) %>% summarise(tot=sum(weight))) %>%
+  ggplot(aes(from_dt, tot)) +
+  geom_point() +
+  geom_smooth()
+
+# -------
